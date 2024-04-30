9f7b1d0f53250ecd61502434d89ed200f09d9f1b
dragoni7 <sdgibson18@outlook.com>
2024-04-30 16:22:16 -0700

    Updated dependencies. Enabled mythicbotany compat. Updated materials to use new harvest tier.

--- src/main/java/com/github/dragoni7/silentcompat/modcompats/MythicBotanyCompat.java
+++ src/main/java/com/github/dragoni7/silentcompat/modcompats/MythicBotanyCompat.java
@@ -1,18 +1,28 @@
 package com.github.dragoni7.silentcompat.modcompats;
 
+import mythicbotany.register.ModItems;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import vazkii.botania.common.entity.ManaBurstEntity;
+import vazkii.botania.common.handler.BotaniaSounds;
+import vazkii.botania.common.item.equipment.tool.terrasteel.TerraBladeItem;
+
 public class MythicBotanyCompat {
-	/*
+
 	public static void SpawnManaBurst(LivingEntity entity) {
         Player player = (Player) entity;
-        if(!entity.getLevel().isClientSide() && !player.isSpectator()) {
+        if(!entity.level().isClientSide() && !player.isSpectator()) {
             if (!player.getMainHandItem().isEmpty()) {
                 ItemStack gearItemStack = player.getMainHandItem();
 
                 if (player.getAttackStrengthScale(0F) == 1) {
                 	ManaBurstEntity burst = getAlfBurst(player, ModItems.alfsteelSword.getDefaultInstance());
-                    player.getLevel().addFreshEntity(burst);
+                    player.level().addFreshEntity(burst);
                     gearItemStack.hurtAndBreak(1, player, p -> p.broadcastBreakEvent(EquipmentSlot.MAINHAND));
-                    player.getLevel().playSound(null, player.getX(), player.getY(), player.getZ(), BotaniaSounds.terraBlade, SoundSource.PLAYERS, 1F, 1F);
+                    player.level().playSound(null, player.getX(), player.getY(), player.getZ(), BotaniaSounds.terraBlade, SoundSource.PLAYERS, 1F, 1F);
                 }
             }
         }
@@ -27,5 +37,4 @@ public class MythicBotanyCompat {
         burst.setManaLossPerTick(2.0F);
         return burst;
 	}
-*/
 }
--- src/main/java/com/github/dragoni7/silentcompat/trait/AlfBurstTrait.java
+++ src/main/java/com/github/dragoni7/silentcompat/trait/AlfBurstTrait.java
@@ -3,9 +3,13 @@ package com.github.dragoni7.silentcompat.trait;
 import java.util.Collection;
 
 import com.github.dragoni7.silentcompat.SilentCompat;
+import com.github.dragoni7.silentcompat.consts.ModConsts;
+import com.github.dragoni7.silentcompat.modcompats.MythicBotanyCompat;
+
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.fml.ModList;
 import net.silentchaos512.gear.gear.trait.SimpleTrait;
 
 public class AlfBurstTrait extends SimpleTrait {
@@ -15,9 +19,9 @@ public class AlfBurstTrait extends SimpleTrait {
     @Override
     public void onItemSwing(ItemStack stack, LivingEntity entity, int traitLevel) {
     	
-    	/*if(ModList.get().isLoaded(ModConsts.MYTHICBOT)) {
+    	if(ModList.get().isLoaded(ModConsts.MYTHICBOT)) {
     		MythicBotanyCompat.SpawnManaBurst(entity);
-    	}*/
+    	}
     }
 
     public AlfBurstTrait(ResourceLocation id) {
